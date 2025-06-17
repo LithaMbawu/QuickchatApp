215cd990d314ec07f55b7bfb9def8a2ec5c25bfd
LithaMbawu <st10486617@rcconnect.edu.za>
2025-06-17 21:49:52 +0200

    Add files via upload

---
+++ QuickChatApp.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sendingmessage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lithambawu
+ */
+class QuickChatApp {
+
+    static ArrayList<Object> sentMessages;
+    static ArrayList<Object> messageHashes;
+
+    static void displayLongestMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Message getLongestMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static class Message {
+
+        public Message(String john, String short_message) {
+        }
+
+        Object getText() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        Object getHash() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+
+        Object getRecipient() {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+    }
+    
+}
